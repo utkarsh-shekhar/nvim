7ca1d7460d43a9dc7e38b3985f84fd9ca906f6e2
Utkarsh Shekhar <iamutkarshshekhar@gmail.com>
2024-07-28 17:23:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neovim config
+
+Prerequisite: install [ripgrep](https://github.com/BurntSushi/ripgrep).
+
+Heavily inspired from ThePrimeagen's [init.lua](https://github.com/ThePrimeagen/init.lua]
---
+++ lua/utkarsh/init.lua
@@ -0,0 +1,33 @@
+require("config.set")
+require("config.remap")
+require("config.lazy")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    callback = function(e)
+    local opts = { buffer = e.buf }
+
+    -- main lsp features
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
+    vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    -- diagnostic
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
+-- netrw settings
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
